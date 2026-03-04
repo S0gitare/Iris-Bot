edc41492190f633a7e4505a4de2122ae2749b64f
S0gitare <alecsanderdavinci@gmail.com>
2026-03-03 22:01:59 -0300

    refatora a inicialização do banco de dados e adiciona operações de salvar e ler anotações

--- src/database/data.py
+++ src/database/data.py
@@ -2,24 +2,49 @@ import sqlite3
 import sys
 
 
-class Database:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.connection = None
+def Database_init(db_file):
+    try:
+        connection = sqlite3.connect("Data.db")
+        cursor = connection.cursor()
 
-        def connect(self):
-            try:
-                self.connection = sqlite3.connect(self.db_file)
-            except sqlite3.Error as e:
-                print(f"Error connecting to database: {e}")
-                sys.exit(1)
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS Anotacoes (
+                comando TEXT PRIMARY KEY,
+                texto TEXT NOT NULL)"""
+        )
+        return connection, cursor
 
-        def cursor(self):
-            if self.connection is None:
-                self.connect()
+    except sqlite3.Error as e:
+        print("Erro ao se conextar ao banco de dados")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    connection, cursor = Database_init("Data.db")
+
+    try:
+        acao = sys.argv[1]
+
+        if acao == "salvar":
+            chave = sys.argv[2]
+            valor = sys.argv[3]
             cursor.execute(
-                """CREATE TABLE IF NOT EXISTS Anotacoes (
-        comando TEXT PRIMARY KEY,
-        texto TEXT NOT NULL"""
+                "REPLACE INTO Anotacoes (comando, texto) VALUES (?, ?)", (chave, valor)
             )
-            return self.connection.cursor()
+            connection.commit()
+        elif acao == "ler":
+            chave = sys.argv[2]
+            cursor.execute("SELECT texto FROM Anotacoes WHERE comando = ?", (chave))
+            result = cursor.fetchone()
+
+            if result:
+                print(result[0])
+
+            else:
+                print("erro")
+
+    except IndexError:
+        print("Erro: Faltam argumentos. Use: python banco.py salvar <chave> <valor>")
+
+    finally:
+        connection.close()
